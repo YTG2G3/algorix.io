d535800a1cbc762f4970f89958c1e393d093bb60
YTG2G3 <scratchlover05@gmail.com>
2024-02-21 02:22:43 -0800

    readme and design

---
+++ lib/fonts.ts
@@ -0,0 +1,15 @@
+import { Inter } from 'next/font/google';
+import localFont from 'next/font/local';
+
+// Inter is default font, SUIT is for emphasis
+// Use `font-sans` and `font-suit` in Tailwind CSS (see tailwind.config.ts)
+
+export const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter'
+});
+
+export const suit = localFont({
+  src: './SUIT-Variable.woff2',
+  variable: '--font-suit'
+});
